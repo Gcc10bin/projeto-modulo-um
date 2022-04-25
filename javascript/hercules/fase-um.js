7be1490f138f403af5c181c0afb4aef0254e029c
gabrielcchrist@gmail.com <gabrielcchrist@gmail.com>
2022-04-24 22:38:30 -0300

    primeiro commit do jogo

---
+++ javascript/hercules/fase-um.js
@@ -0,0 +1,19 @@
+function validacaoDeDados(){
+    var escolhaUmaOpcao = prompt('Tome uma decisão: opção 1 ou 2?');
+    while(true){
+        if(escolhaUmaOpcao == 1 || escolhaUmaOpcao == 2){
+            break
+        } else{
+            alert("invalido. escolha entre a opção 1 ou 2!");
+            escolhaUmaOpcao = prompt("Tome uma decisao: opcao 1 ou 2?");
+        } 
+    } function proximaFase(){
+        if(escolhaUmaOpcao == '1'){
+            window.location.href = 'game-over.html';
+        } else{
+            window.location.href = 'fase-dois.html';
+        }
+    }   proximaFase();
+}
+
+
